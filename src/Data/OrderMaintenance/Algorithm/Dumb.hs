170fbc7b19fdf5035d24e60a1681415d35a3fd57
Wolfgang Jeltsch <wolfgang-it@jeltsch.info>
2014-12-02 11:23:43 +0000

    changed the dumb algorithm to use Haskell’s Rational type

--- src/Data/OrderMaintenance/Algorithm/Dumb.hs
+++ src/Data/OrderMaintenance/Algorithm/Dumb.hs
@@ -12,7 +12,7 @@ import Control.Monad.ST
 -- Data
 
 import           Data.Function
-import           Data.Bits
+import           Data.Ratio
 import           Data.STRef
 import qualified Data.Set as Set
 import           Data.Set (Set)
@@ -30,34 +30,7 @@ type instance ElementCell Dumb s = PureElement
 
 type PureOrder = Set PureElement
 
-type PureElement = [Bit]
-
-oneHalf :: PureElement
-oneHalf = [True]
-
-averageWithZero :: PureElement -> PureElement
-averageWithZero pureElem = False : pureElem
-
-averageWithOne :: PureElement -> PureElement
-averageWithOne pureElem = True : pureElem
-
-average :: PureElement -> PureElement -> PureElement
-average pureElem1 pureElem2 = carry : sum where
-
-    (carry,sum) = add pureElem1 pureElem2
-
-add :: PureElement -> PureElement -> (Bit,PureElement)
-add []             pureElem2      = (False,pureElem2)
-add pureElem1      []             = (False,pureElem1)
-add (bit1 : bits1) (bit2 : bits2) = (carry,sum) where
-
-    (carry',sum') = add bits1 bits2
-
-    carry = bit1 && bit2 || bit1 && carry' || bit2 && carry'
-
-    sum = bit1 `xor` bit2 `xor` carry' : sum'
-
-type Bit = Bool
+type PureElement = Rational
 
 rawAlgorithm :: RawAlgorithm Dumb s
 rawAlgorithm = RawAlgorithm {
@@ -99,29 +72,29 @@ relative conv trans rawElem rawOrder = do
 
 pureInsertMinimum :: PureOrder -> PureElement
 pureInsertMinimum pureOrder
-    | Set.null pureOrder = oneHalf
-    | otherwise          = averageWithZero (Set.findMin pureOrder)
+    | Set.null pureOrder = 1 % 2
+    | otherwise          = Set.findMin pureOrder / 2
 
 pureInsertMaximum :: PureOrder -> PureElement
 pureInsertMaximum pureOrder
-    | Set.null pureOrder = oneHalf
-    | otherwise          = averageWithOne (Set.findMax pureOrder)
+    | Set.null pureOrder = 1 % 2
+    | otherwise          = (Set.findMax pureOrder + 1) / 2
 
 pureInsertAfter :: PureElement -> PureOrder -> PureElement
 pureInsertAfter pureElement pureOrder = pureElement' where
 
     greater = snd (Set.split pureElement pureOrder)
 
-    pureElement' | Set.null greater = averageWithOne pureElement
-                 | otherwise        = average pureElement (Set.findMin greater)
+    pureElement' | Set.null greater = (pureElement + 1) / 2
+                 | otherwise        = (pureElement + Set.findMin greater) / 2
 
 pureInsertBefore :: PureElement -> PureOrder -> PureElement
 pureInsertBefore pureElement pureOrder = pureElement' where
 
     lesser = fst (Set.split pureElement pureOrder)
 
-    pureElement' | Set.null lesser = averageWithZero pureElement
-                 | otherwise       = average pureElement (Set.findMax lesser)
+    pureElement' | Set.null lesser = pureElement / 2
+                 | otherwise       = (pureElement + Set.findMax lesser) / 2
 
 pureDelete :: PureElement -> PureOrder -> ((),PureOrder)
 pureDelete pureElement pureOrder = ((),Set.delete pureElement pureOrder)
