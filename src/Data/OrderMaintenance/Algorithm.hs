47b08615b1a167fc07908ca01cb8bcb0650b40bd
Wolfgang Jeltsch <wolfgang-it@jeltsch.info>
2014-12-05 16:12:35 +0000

    added remark regarding the implementation of Bender et al.

--- src/Data/OrderMaintenance/Algorithm.hs
+++ src/Data/OrderMaintenance/Algorithm.hs
@@ -30,6 +30,19 @@ import Data.OrderMaintenance.Algorithm.Dumb as Dumb
     Bender et al. as an order maintenance algorithm in its own right.
 -}
 
+{-FIXME:
+    For implementing Bender et al., it might be good to store the calibrator
+    tree in an array, level by level from top to bottom. The array must then be
+    created without initializing its elements. Initially the tree would be
+    small; so few array elements would be used. When extending the tree, we
+    would face the problem that initializing all the additionally used elements
+    would take more than O(1) time. We can maybe use the trick by Barak A.
+    Pearlmutter¹ (or a variant of it, specialized for our particular
+    initialization pattern) to get O(1) time.
+
+      ¹ See his e-mail to me from 5 December 2014.
+-}
+
 -- * General things
 
 -- NOTE: Algorithm is imported from Data.OrderMaintenance.Algorithm.Type.
