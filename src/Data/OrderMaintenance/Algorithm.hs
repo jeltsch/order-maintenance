094ddd43e78a7a6a0ad56205ca554b2ba97187e1
Wolfgang Jeltsch <wolfgang-it@jeltsch.info>
2014-12-06 18:02:26 +0000

    added some more notes regarding the implementation of Bender et al.

--- src/Data/OrderMaintenance/Algorithm.hs
+++ src/Data/OrderMaintenance/Algorithm.hs
@@ -43,6 +43,33 @@ import Data.OrderMaintenance.Algorithm.Dumb as Dumb
       ¹ See his e-mail to me from 5 December 2014.
 -}
 
+{-FIXME:
+    More notes regarding implementing Bender et al.:
+
+      • We can store the set of all children of a single node of a log-tree in
+        an array of 48 64-bit words. Each word represents one child. Children
+        are stored in the temporal order of their allocation. 48 bits of a word
+        are the label, 3 are the left sibling index, 3 are the right sibling
+        index. The parent pointer (pointer to the array plus index in the array)
+        has to be stored only once per such an array, not for every child.
+
+      • A block in the file maintenance data structure could encompass 48 or
+        maybe also 64 elements. A 64-bit word could be used to store which of
+        the array cells are taken by an element and which are free.
+
+      • I think that on the upper two levels of a log tree, we need up to three
+        times as many nodes for storing log-many subtrees, because of overflow
+        nodes. This would mean that with the above approach, we could store up
+        to 48 × 12 × 12 ≈ 7000 elements in a log tree and ca. 7000 × 48 ≈ 350000
+        actual elements per file maintenance block. The total memory use would
+        be a bit more than 8 × 350000 = 2.8 MB.
+
+      • The number of actual elements per file maintenance block (350,000) would
+        be a bit more than 2^18. Since our k would be 48, we could have up to
+        2^48 × 2^18 = 2^66 elements theoretically. So we could reach the maximum
+        of 2^64 elements.
+-}
+
 -- * General things
 
 -- NOTE: Algorithm is imported from Data.OrderMaintenance.Algorithm.Type.
