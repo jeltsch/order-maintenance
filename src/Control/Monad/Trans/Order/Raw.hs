8d3935817a9bde0aef5524c61db7adc7f7eacae3
Wolfgang Jeltsch <wolfgang-it@jeltsch.info>
2015-05-07 21:11:59 +0000

    added FIXME regarding algorithms and referential transparency

--- src/Control/Monad/Trans/Order/Raw.hs
+++ src/Control/Monad/Trans/Order/Raw.hs
@@ -37,3 +37,12 @@ data RawAlgorithm o s = RawAlgorithm {
     newBefore       :: RawElement o s -> RawOrder o s -> ST s (RawElement o s),
     delete          :: RawElement o s -> RawOrder o s -> ST s ()
 }
+{-FIXME:
+    If we ever allow users to plug in their own algorithms, we have to flag the
+    respective function as unsafe and point out that referential transparency is
+    in danger if the algorithm does not fulfill the specification. This is
+    because element comparison is presented to the user as a pure function. The
+    important condition is that for any two elements, compareElements must
+    always return the same result as long as delete is not called on either
+    element.
+-}
